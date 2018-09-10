ef12b2eb2ce46db02dac55b0cbbc92bc385f39ec
Little <LittleMa@ad.reyrey.com>
2018-09-10 10:23:01 -0500

    update

---
+++ frontend/src/containers/Gallery.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class Gallery extends Component {
+    render()  {
+        return (
+            <React.Fragment>
+                <h1></h1>
+            </React.Fragment>
+        )
+    }
+}
+
+
+export default Gallery;

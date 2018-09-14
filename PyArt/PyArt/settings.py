a0089b258039fb7be619650e51fa98a4453523f3
Ubuntu <ubuntu@ip-172-31-42-46.us-east-2.compute.internal>
2018-09-14 16:07:03 +0000

    deployment

--- PyArt/PyArt/settings.py
+++ PyArt/PyArt/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'h7m=+v#ddur*-93+vs@!=d7vxp+z30k7l#zyni9ee-)9ot%-f&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
